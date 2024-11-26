d6658da0c3f5631d8a60705f5690d4382d642a4e
erisbarrancop <ibarpri1712@iesfuengirola1.es>
2024-11-26 16:38:10 +0100

    Displaying a check or an x symbol depending on whether it's the correct or incorrect answer

--- build/view.asset.php
+++ build/view.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '79955947429d6bf4cc06', 'type' => 'module');
+<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => '793bad1fbe9159b2aab1', 'type' => 'module');
