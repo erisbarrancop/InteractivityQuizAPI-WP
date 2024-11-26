d26e9f3a4e7706e888621d9b1b383d9d3d26b9f1
erisbarrancop <ibarpri1712@iesfuengirola1.es>
2024-11-26 13:02:25 +0100

    Implemented all of the necessary code for the quiz to show up in the admin editor screen

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => 'e6a7e7298bc2b4066ce6');
+<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'bcbed98698190c2f3b28');
--- src/edit.js
+++ src/edit.js
@@ -1,3 +1,7 @@
+import {TextControl, Flex, FlexBlock, FlexItem, Button, Icon, PanelBody, PanelRow, ColorPicker} from "@wordpress/components"
+import {InspectorControls, BlockControls, AlignmentToolbar} from "@wordpress/block-editor"
+import {ChromePicker} from "react-color"
+
 /**
  * Retrieves the translation of text.
  *
@@ -25,15 +29,70 @@ import { useBlockProps } from '@wordpress/block-editor';
  *
  * @return {Element} Element to render.
  */
-export default function Edit( { attributes, setAttributes } ) {
+export default function Edit(props) {
 	const blockProps = useBlockProps();
 
+	function updateQuestion(value){
+		props.setAttributes({question: value})
+	  }
+	  
+	  function deleteAnswer(indexToDelete) {
+		const newAnswers = props.attributes.answers.filter(function(x, index) {
+		  return index != indexToDelete
+		})
+		props.setAttributes({answers: newAnswers})
+	
+		if(indexToDelete == props.attributes.correctAnswer){
+		  props.setAttributes({correctAnswer: undefined})
+		}
+	  }
+	
+	  function markAsCorrect(index){
+		props.setAttributes({correctAnswer: index})
+	
+	  }
+
 	return (
-		<p { ...blockProps }>
-			{ __(
-				'Interactivity Quiz – hello from the editor!',
-				'interactivity-quiz'
-			) }
-		</p>
-	);
+		<div {...blockProps}>
+		<div className="paying-attention-edit-block" style={{backgroundColor : props.attributes.bgColor}}>
+			<BlockControls>
+			  <AlignmentToolbar value={props.attributes.theAlignment} onChange ={x => props.setAttributes({theAlignment: x})} />
+			</BlockControls>
+			<InspectorControls>
+			  <PanelBody title = "Background Color" initialOpen={true}>
+			  <PanelRow>
+				<ChromePicker color={props.attributes.bgColor} onChangeComplete={x => props.setAttributes({bgColor: x.hex})} disableAlpha={true} />
+			  </PanelRow>
+			</PanelBody>
+			</InspectorControls>
+		  <TextControl label ="Question:" value={props.attributes.question} onChange={updateQuestion}style={{fontSize:"20px"}}/>
+		  <p style={{fontSize: "13px", margin: "20px 0 8px 0"}} >Answers:</p>
+		  {props.attributes.answers.map(function (answer, index) {
+			return (
+			  <Flex>
+			<FlexBlock>
+			  <TextControl value={answer} onChange={newValue => {
+				const newAnswers = props.attributes.answers.concat([]) 
+				newAnswers[index] = newValue
+				props.setAttributes({answers: newAnswers})
+			  }} />
+			</FlexBlock>
+			<FlexItem>
+			  <Button onClick = {() => markAsCorrect(index)}>
+				<Icon className="mark-as-correct" icon={props.attributes.correctAnswer == index ? "star-filled" : "star-empty"}/>
+			  </Button>
+			</FlexItem>
+			<FlexItem>
+			<Button isLink className="attention-delete" onClick={() => deleteAnswer(index)}>Delete</Button>
+			</FlexItem>
+			<FlexItem></FlexItem>
+		  </Flex>
+			)
+		  })}
+		  <Button isPrimary onClick={() => {
+			props.setAttributes({answers: props.attributes.answers.concat([""])})
+		  }}>Add another answer</Button>
+		</div>
+		</div>
+	  );
 }
